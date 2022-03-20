a4a7b1c0496ff4dd6027f49a1c1168e267ce6f67
kingzamzon <adesanoye.samson@yahoo.com>
2022-03-20 02:06:04 +0100

    add getProviderorSigner change export to stake

--- client/constants/index.js
+++ client/constants/index.js
@@ -803,4 +803,4 @@ export const abi = [
       "type": "function"
     }
   ];
-export const WHITELIST_CONTRACT_ADDRESS = "0xd5280e3B17329d8293289dE6BFB1567f53F27F72";
+export const STAKING_CONTRACT_ADDRESS = "0xd5280e3B17329d8293289dE6BFB1567f53F27F72";
